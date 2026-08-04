b67f6346f953838061b56bcba7c770f0b4207d37
KokkallaHithasree <kokkallahithasree@gmail.com>
2026-08-04 03:54:44 -0700

    ADV132

--- backend/src/main/java/com/dbtraining/reconx/repository/AuditLogRepository.java
+++ backend/src/main/java/com/dbtraining/reconx/repository/AuditLogRepository.java
@@ -1,10 +1,18 @@
 package com.dbtraining.reconx.repository;
 
+
 import com.dbtraining.reconx.repository.entity.AuditLogEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface AuditLogRepository extends JpaRepository<AuditLogEntry, Long> {
+
+@Repository
+public interface AuditLogRepository 
+        extends JpaRepository<AuditLogEntry, Long> {
+
+
     List<AuditLogEntry> findByTradeRefOrderByEventTimestampAsc(String tradeRef);
+
 }
